deb26062b529a8e4bed6a10d78fe25fe241cddc2
Cameron Ketcham <cketcham@gmail.com>
2009-09-10 07:09:47 -0700

    inital commit for whatsnoisy campaign

---
+++ src/edu/ucla/cens/whatsnoisy/whatsnoisy.java
@@ -0,0 +1,13 @@
+package edu.ucla.cens.whatsnoisy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class whatsnoisy extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
